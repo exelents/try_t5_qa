ed16c60de353d3cd5207c329ca9526d7224c5e92
Ilia Urkov <exelents.s@gmail.com>
2021-01-10 18:36:42 +0300

    try t5 qa

---
+++ run-qa-11b.sh
@@ -0,0 +1,8 @@
+export BS=1; rm -r output_dir; CUDA_VISIBLE_DEVICES=0 PYTHONPATH=../../src USE_TF=0 deepspeed  \
+./finetune_trainer.py --model_name_or_path google/t5-v1_1-xxl --output_dir output_dir --adam_eps 1e-06 \
+--data_dir qasc --do_eval --do_predict --do_train --evaluation_strategy=steps  --freeze_embeds --label_smoothing 0.1 \
+--learning_rate 3e-5 --logging_first_step --logging_steps 1000 --max_source_length 64 --max_target_length 64 \
+--num_train_epochs 1 --overwrite_output_dir --per_device_eval_batch_size $BS --per_device_train_batch_size $BS \
+--predict_with_generate --eval_steps 25000 --save_steps 25000 --sortish_sampler --src_lang en_XX --task translation \
+--test_max_target_length 64 --tgt_lang ro_RO --val_max_target_length 64 --warmup_steps 500 --n_train 2000 --n_val 2000 \
+--n_test 2000 --deepspeed ds_config.json
