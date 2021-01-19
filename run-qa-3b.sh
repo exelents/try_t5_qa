fb9256a6d746c75fff823970ace2e03f51081a3a
Ilia Urkov <exelents.s@gmail.com>
2021-01-19 23:48:15 +0300

    change training command

--- run-qa-3b.sh
+++ run-qa-3b.sh
@@ -2,8 +2,8 @@ export BS=12; rm -r output_dir; CUDA_VISIBLE_DEVICES=0 PYTHONPATH=../../src USE_
 ./finetune_trainer.py --model_name_or_path google/t5-v1_1-xl --output_dir output_dir --adam_eps 1e-06 \
 --data_dir qasc --do_eval --do_predict --do_train --evaluation_strategy=steps  --freeze_embeds --label_smoothing 0.1 \
 --learning_rate 3e-5 --logging_first_step --logging_steps 50 --max_source_length 100 --max_target_length 70 \
---num_train_epochs 2 --overwrite_output_dir --per_device_eval_batch_size $BS --per_device_train_batch_size $BS \
---predict_with_generate --eval_steps 25000 --save_steps 10000 --sortish_sampler --src_lang en_XX --task translation \
+--num_train_epochs 4 --overwrite_output_dir --per_device_eval_batch_size $BS --per_device_train_batch_size $BS \
+--predict_with_generate --eval_steps 25000 --save_steps 6668 --sortish_sampler --src_lang en_XX --task translation \
 --test_max_target_length 70 --tgt_lang ro_RO --val_max_target_length 70 --warmup_steps 500 --n_train 40000 --n_val 2000 \
 --n_test 2000 --deepspeed ds_config.json
 # google/t5-v1_1-xl    BS=14
